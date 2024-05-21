ccb3be82307036173b35b2926e0ec9bb54fb8350
fruitxxsnackz <Joanna1162494@gmail.com>
2024-05-20 17:55:24 -0700

    Updating models

--- Module18HW/routes/api/routes.js
+++ Module18HW/routes/api/routes.js
@@ -1,30 +1,23 @@
 const router = require('express').Router();
 
 const {
-    getthoughts,
-    thoughtsID,
-    newthoughts,
-    refreshthoughts,
-    deleteThought,
-    addingreact,
-    deletingreact
+   getthoughts,
+   thoughtsID,
+   newthoughts,
+   refreshthoughts,
+   deleteThought,
+   addingreact, 
+   deletingreact
 } = require('../../controllers/thoughtscontroller');
 
-router.route('/')
-.get(getthoughts)
+router.route('/').get(getthoughts)
 
-router.route('/:userId')
-.post(newthoughts)
+router.route('/:userId').post(newthoughts)
 
-router.route('/:thoughtId')
-.get(thoughtsID)
-.put(refreshthoughts)
-.delete(deleteThought)
+router.route('/:thoughtId').get(thoughtsID).put(refreshthoughts).delete(deleteThought)
 
-router.route('/:thoughtId/reactions')
-.post(addingreact)
+router.route('/:thoughtId/reactions').post(addingreact)
 
-router.route('/:thoughtId/reactions/:reactionId')
-.delete(deletingreact)
+router.route('/:thoughtId/reactions/:reactionId').delete(deletingreact)
 
 module.exports = router;
--- Module18HW/routes/api/userroute.js
+++ Module18HW/routes/api/userroute.js
@@ -1,26 +1,19 @@
 const router = require('express').Router();
 
 const {
-    getuser,
-    addusers,
-    userIDs,
-    userupdates,
-    deleteusers,
-    addingusers,
-    deletingusers
+  getuser,
+  addusers,
+  userIDs,
+  userupdates,
+  deleteusers,
+  addingusers,
+  deletingusers
 } = require('../../controllers/usercontroller');
 
-router.route('/')
-.get(getuser)
-.post(addusers);
+router.route('/').get(getuser).post(addusers);
 
-router.route('/:id')
-.get(userIDs)
-.put(userupdates)
-.delete(deleteusers);
+router.route('/:id').get(userIDs).put(userupdates).delete(deleteusers);
 
-router.route('/:id/friends/:friendID')
-.post(addingusers)
-.delete(deletingusers)
+router.route('/:id/friends/:friendID').post(addingusers).delete(deletingusers)
 
 module.exports = router;
